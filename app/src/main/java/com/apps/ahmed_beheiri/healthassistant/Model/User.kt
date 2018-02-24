efff6d02bd5ca6f646a13856e8c7fb3cdeee4463
Ahmed Beheiri <ahmedbeheirii@gmail.com>
2018-02-24 10:24:44 +0200

    add Authantication with E-mail

--- app/src/main/java/com/apps/ahmed_beheiri/healthassistant/Model/User.kt
+++ app/src/main/java/com/apps/ahmed_beheiri/healthassistant/Model/User.kt
@@ -1,9 +1,15 @@
 package com.apps.ahmed_beheiri.healthassistant.Model
 
+import android.annotation.SuppressLint
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
 /**
  * Created by ahmed_beheiri on 23/02/18.
  */
-data class User(var email:String,var imageuri:String,var username:String) {
+@SuppressLint("ParcelCreator")
+@Parcelize
+data class User(var email:String,var imageuri:String,var username:String): Parcelable {
     constructor(email:String, imageuri:String, username:String,followers :ArrayList<User>) : this(email,imageuri, username)
 
 }
--- app/src/main/java/com/apps/ahmed_beheiri/healthassistant/UI/MainActivity.kt
+++ app/src/main/java/com/apps/ahmed_beheiri/healthassistant/UI/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.apps.ahmed_beheiri.healthassistant.UI
 
+import android.app.Activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -21,8 +22,12 @@ import android.support.annotation.NonNull
 import com.apps.ahmed_beheiri.healthassistant.Model.Contract
 import com.apps.ahmed_beheiri.healthassistant.Model.User
 import com.google.android.gms.tasks.OnCompleteListener
-import com.apps.ahmed_beheiri.healthassistant.R.drawable.email
-import com.apps.ahmed_beheiri.healthassistant.R.drawable.email
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DataSnapshot
+import android.provider.MediaStore
 
 
 
@@ -32,6 +37,8 @@ import com.apps.ahmed_beheiri.healthassistant.R.drawable.email
 class MainActivity : AppCompatActivity() {
 
     lateinit var mAuth:FirebaseAuth
+    lateinit var databaseref:DatabaseReference
+    lateinit var database:FirebaseDatabase
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,12 +46,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         configureFabreveal(fab_reveal_layout)
         mAuth= FirebaseAuth.getInstance()
+
+       database = FirebaseDatabase.getInstance()
+
+
         cancel.setOnClickListener { prepareBackTransation(fab_reveal_layout) }
         signInButton.setOnClickListener{
             if(emaillogin.text.isEmpty()||passlogin.text.isEmpty()){
                 Toast.makeText(this@MainActivity,"please Enter your e-mail and Password",Toast.LENGTH_LONG).show()
             }else {
                 signInEmail(emaillogin.text.toString(),passlogin.text.toString())
+            }
+        }
+
+        pickimage.setOnClickListener{
+            uploadImage()
+        }
+        signupbtn.setOnClickListener{
+            if(signupemail.text.isEmpty()||passsignup.text.isEmpty()||imageuri.text.equals("Pick an Image")){
+                Toast.makeText(this@MainActivity,"Please enter E-mail and password and pick an image to signup",Toast.LENGTH_LONG).show()
+
+            }else{
+                signUp(signupemail.text.toString(),passsignup.text.toString(),imageuri.text.toString())
             }
         }
 
@@ -56,7 +79,18 @@ class MainActivity : AppCompatActivity() {
         super.onStart()
         if(mAuth.currentUser!=null){
             val currentuser= mAuth.currentUser
-            updateUI(currentuser)
+            databaseref=database.getReference(currentuser?.uid)
+            databaseref.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    var myuser:User= dataSnapshot.value as User
+                    updateUI(myuser)
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    // Failed to read value
+                    Log.w("Ch3", "Failed to read value.", error.toException())
+                }
+            })
         }
     }
 
@@ -80,12 +114,25 @@ class MainActivity : AppCompatActivity() {
 
     private fun signInEmail(email:String,pass:String){
 
-        mAuth.createUserWithEmailAndPassword(email, pass)
+        mAuth.signInWithEmailAndPassword(email, pass)
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
                         // Sign in success, update UI with the signed-in user's information
                         val user = mAuth.currentUser
-                        updateUI(user)
+                        databaseref=database.getReference(user?.uid)
+                        databaseref.addValueEventListener(object : ValueEventListener {
+                            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                                var myuser:User= dataSnapshot.value as User
+                                updateUI(myuser)
+                            }
+
+                            override fun onCancelled(error: DatabaseError) {
+                                // Failed to read value
+                                Log.w("Ch3", "Failed to read value.", error.toException())
+                            }
+                        })
+
+
                     } else {
                         // If sign in fails, display a message to the user.
 
@@ -98,14 +145,18 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    private fun signUp(email: String,pass: String){
-        mAuth.signInWithEmailAndPassword(email, pass)
+    private fun signUp(email: String,pass: String,imageuri:String){
+        mAuth.createUserWithEmailAndPassword(email, pass)
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
                         // Sign in success, update UI with the signed-in user's information
 
                         var user = mAuth.currentUser
-                        updateUI(user)
+                        databaseref=database.getReference(user?.uid)
+                        var myuser:User=User(email,pass,imageuri)
+                        databaseref.setValue(user)
+
+                        updateUI(myuser)
                     } else {
                         // If sign in fails, display a message to the user.
                         Toast.makeText(this@MainActivity, "signup failed.",
@@ -119,13 +170,60 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    private fun updateUI(user:FirebaseUser?){
+    private fun updateUI(user:User?){
         if (user!=null) {
-            var user1:User= User(user.email!!,user.photoUrl.toString()!!, user.displayName!!)
+            var user1:User=user
             var i: Intent = Intent(this@MainActivity, TransactionActivity::class.java)
-           // i.putExtra(Contract.EXTRA_USER_VALUE1,user1)
+            i.putExtra(Contract.EXTRA_USER_VALUE1,user1)
             startActivity(i)
         }
 
+    }
+
+    private fun ReadUser(UID:String):User?{
+        var user:User?=null
+        databaseref=database.getReference(UID)
+        databaseref.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                user= dataSnapshot.value as User
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w("Ch3", "Failed to read value.", error.toException())
+            }
+        })
+
+        return user
+
+    }
+
+
+    private fun uploadImage(){
+        val takePicture = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(takePicture, 0)
+        val pickPhoto = Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(pickPhoto, 1)
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when(requestCode){
+            0 ->{
+                if(resultCode== Activity.RESULT_OK){
+                    val selectedImage = data?.data
+                    imageuri.text = selectedImage.toString()
+                }
+            }
+
+            1 ->{
+                if (resultCode== Activity.RESULT_OK){
+                    val selectedImage = data?.data
+                    imageuri.text = selectedImage.toString()
+                }
+            }
+        }
     }
 }
